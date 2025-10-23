a24eef736bc3e2686520c8488188e57044bbadbe
Samatha Pulgam <pulgamsamatha@gmail.com>
2025-10-23 11:43:08 +0530

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,116 +1,13 @@
-import { Component } from "react";
-import { Navigate } from "react-router-dom"; // 01 use Navigate from react-router-dom
-
 import "./index.css";
 
-class LoginForm extends Component {
-  state = {
-    username: "",
-    password: "",
-    redirect: false,
-  };
+const NotFound = () => (
+  <div className="not-found-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      alt="not-found"
+      className="not-found-img"
+    />
+  </div>
+);
 
-  // onSubmitSuccess = () => {
-  //   const { history } = this.props;
-  //   history.replace("/");
-  // };
-
-  submitForm = (event) => {
-    event.preventDefault();
-    const { username, password } = this.state;
-    const userDetails = { username, password };
-    // const url = 'https://apis.ccbp.in/login'
-    // const options = {
-    //   method: 'POST',
-    //   body: JSON.stringify(userDetails),
-    // }
-    // const response = await fetch(url, options)
-    // const data = await response.json()
-    // if (response.ok === true) {
-    //   this.onSubmitSuccess()
-    // }
-
-    event.preventDefault();
-    localStorage.setItem("userData", JSON.stringify(userDetails));
-    this.setState({ redirect: true });
-  };
-
-  onChangeUsername = (event) => {
-    this.setState({ username: event.target.value });
-  };
-
-  onChangePassword = (event) => {
-    this.setState({ password: event.target.value });
-  };
-
-  renderPasswordField = () => {
-    const { password } = this.state;
-    return (
-      <>
-        <label className="input-label" htmlFor="password">
-          PASSWORD
-        </label>
-        <input
-          type="password"
-          id="password"
-          className="password-input-filed"
-          value={password}
-          onChange={this.onChangePassword}
-        />
-      </>
-    );
-  };
-
-  renderUsernameField = () => {
-    const { username } = this.state;
-    return (
-      <>
-        <label className="input-label" htmlFor="username">
-          USERNAME
-        </label>
-        <input
-          type="text"
-          id="username"
-          className="username-input-filed"
-          value={username}
-          onChange={this.onChangeUsername}
-        />
-      </>
-    );
-  };
-
-  render() {
-    if (this.state.redirect) {
-      return <Navigate to="/home" />;
-    }
-
-    return (
-      <div className="login-form-container">
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
-          className="login-website-logo-mobile-image"
-          alt="website logo"
-        />
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
-          className="login-image"
-          alt="website login"
-        />
-        <form className="form-container" onSubmit={this.submitForm}>
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
-            className="login-website-logo-desktop-image"
-            alt="website logo"
-          />
-          <div className="input-container">{this.renderUsernameField()}</div>
-          <div className="input-container">{this.renderPasswordField()}</div>
-          <button type="submit" className="login-button">
-            Login
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
-
-export default LoginForm;
+export default NotFound;
