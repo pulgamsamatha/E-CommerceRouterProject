0440af4721e8c2748c8b239e8dddb49dd7d0cf22
Samatha Pulgam <pulgamsamatha@gmail.com>
2025-10-23 11:45:16 +0530

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,13 +1,17 @@
-import "./index.css";
+import Header from '../Header'
+import './index.css'
 
-const NotFound = () => (
-  <div className="not-found-container">
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
-      alt="not-found"
-      className="not-found-img"
-    />
-  </div>
-);
+const Products = () => (
+  <>
+    <Header />
+    <div className="products-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="products-img"
+      />
+    </div>
+  </>
+)
 
-export default NotFound;
+export default Products
